9509170eacb7c468a987e9abb2f96f92a5af05aa
toscar7 <91301875+toscar7@users.noreply.github.com>
2021-12-06 09:53:42 -0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "onebucket" {
     bucket         = "demo-s3-bucket-use-terraform"
 
         tags = {
-	      Name = "s3"
+	      Name = "s3-bucket"
 	          }  
 		  }
 
